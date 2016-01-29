b8eed7911e45fab31d8b2aa710becf7aa7d0f205
Muaad <muadh24@gmail.com>
2016-01-29 16:01:47 +0300

    Adding options

---
+++ options.js
@@ -0,0 +1,28 @@
+// Saves options to chrome.storage
+function save_options() {
+  var endpointURL = document.getElementById('endpointURL').value;
+  chrome.storage.sync.set({
+    endpointURL
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'URL saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get({
+    endpointURL: 'Your endpoint URL here'
+  }, function(items) {
+    document.getElementById('endpointURL').value = items.endpointURL;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
