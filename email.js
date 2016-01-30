293f9d55455dcbd7a3c1e39844cddb288deb6cb3
Muaad <muadh24@gmail.com>
2016-01-30 10:40:12 +0300

    Managed to read endpointURL from storage and use it to make requests

--- email.js
+++ email.js
@@ -1,26 +1,35 @@
+function getURL(fn){
+	chrome.storage.sync.get('endpointURL', function(items) {
+	  fn(items.endpointURL);
+	});
+}
+
 function getEmail() {
 	setTimeout(function () {
 		var emailDiv = document.querySelector (".cdc-inbox-userinfo-box-inner-email  > :first-child")
 		var email = document.querySelector (".cdc-inbox-userinfo-box-inner-email  > :first-child").innerHTML
-		$.ajax({
-		  type: "GET",
-		  url: "https://ongair.im/api/v2/stats/user?email=" + email,
-		  dataType: 'json',
-		  success: function(data, textStatus, jqXhr) {
-		  	// showIfEmailIsRegistered(document.querySelector (".cdc-inbox-userinfo-box-inner-email  > :first-child"), data)
-		  	if (data.exists) {
-		  		emailDiv.innerHTML = email + " | <span style='color: green;'>YES</span>"
-		  	} else{
-		  		emailDiv.innerHTML = email + " | <span style='color: red;'>NO</span>"
-		  	};
-		    console.log(data);
-		  },
-		  error: function (request, status, error) {
-		    console.log(request.responseText);
-		  }
-		});
-        console.log(email)
-    }, 500);	
+		
+		getURL(function(url){
+			$.ajax({
+			  type: "GET",
+			  url: url + "?email=" + email,
+			  dataType: 'json',
+			  success: function(data, textStatus, jqXhr) {
+			  	// showIfEmailIsRegistered(document.querySelector (".cdc-inbox-userinfo-box-inner-email  > :first-child"), data)
+			  	if (data.exists) {
+			  		emailDiv.innerHTML = email + " | <span style='color: green;'>YES</span>"
+			  	} else{
+			  		emailDiv.innerHTML = email + " | <span style='color: red;'>NO</span>"
+			  	};
+			    console.log(data);
+			  },
+			  error: function (request, status, error) {
+			    console.log(request.responseText);
+			  }
+			});
+	        console.log(email)
+	    }, 500);
+	  });	
 }
 
 window.addEventListener ("load", checkEmail, false);
@@ -29,15 +38,15 @@ function checkEmail (evt) {
     var jsInitChecktimer = setInterval (checkForJS_Finish, 15000);
 
     function checkForJS_Finish () {
-        if (document.querySelector (".cdc-inbox-userinfo-box-inner-email  > :first-child") != null) {
-	            clearInterval (jsInitChecktimer);
-	            getEmail();
+      if (document.querySelector (".cdc-inbox-userinfo-box-inner-email  > :first-child") != null) {
+            clearInterval (jsInitChecktimer);
+            getEmail();
 
-	            var elements = document.getElementsByClassName("cdc-inbox-list-items-item-button");
+            var elements = document.getElementsByClassName("cdc-inbox-list-items-item-button");
 
-	            for (var i = 0; i < elements.length; i++) {
-	                elements[i].addEventListener('click', getEmail, false);
-	            }
-        }
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].addEventListener('click', getEmail, false);
+            }
+      }
     }
 }
