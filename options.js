7bd9f53553d4bc268dd900077db91d785c177415
Muaad <muadh24@gmail.com>
2016-01-30 11:15:26 +0300

    Some more cleanup

--- options.js
+++ options.js
@@ -6,7 +6,7 @@ function save_options() {
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
-    status.className += " alert alert-success";
+    status.className = "alert alert-success";
     status.textContent = 'URL successfully saved.';
     setTimeout(function() {
       status.className = ""
@@ -17,7 +17,6 @@ function save_options() {
 
 // Restores URL field using the preferences stored in chrome.storage.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
     endpointURL: ''
   }, function(items) {
